6594311f728399bc5130497e093640eed8f79000
Mohamed <gassma803@gmail.com>
2025-07-13 00:26:58 +0100

    introduction à StreamBuilder pour accès à ecran en fonction de l'etat de connexion

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
@@ -22,9 +20,34 @@ class App extends StatelessWidget {
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
       ),
-      home: const AuthScreen()
+      home: StreamBuilder(
+        // stream : ici on écoute un flux (Stream) qui vient de FirebaseAuth.
+        // FirebaseAuth.instance.authStateChanges() est un flux qui émet un événement
+        // chaque fois qu'il y a un changement d'état d'authentification (connexion / déconnexion)
+        stream: FirebaseAuth.instance.authStateChanges(),
+
+        // builder : c'est une fonction qui sera appelée automatiquement chaque fois que
+        // le Stream émet une nouvelle valeur (comme un listener)
+        builder: (ctx, snapshot) {
+          // snapshot : représente la dernière valeur (ou l’état) reçue du Stream
+          // Il a plusieurs propriétés utiles :
+          // - hasData : vrai si des données sont arrivées
+          // - data : la donnée elle-même (ici un User? ou null)
+          // - connectionState : état du stream (waiting, active, done...)
+
+          // Si snapshot.hasData est vrai, ça veut dire que l'utilisateur est connecté
+          if (snapshot.hasData) {
+            // On renvoie donc l'écran de chat
+            return ChatScreen();
+          }
+
+          // Sinon, pas connecté, on renvoie l'écran d'authentification
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
