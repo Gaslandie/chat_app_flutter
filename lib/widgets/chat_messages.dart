7b301289c6baae3d286a98ebfeed5fef58f3f98f
Mohamed <gassma803@gmail.com>
2025-07-13 13:16:34 +0100

    Envoi du message vers fireStore. recuperation des message depuis fireStore et affichage dans lapp

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,9 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
   @override
+  //build qui construit l'interface graphique du widget
   Widget build(BuildContext context) {
-    return const Center(child: Text('No messages found.'));
+    return StreamBuilder( //StreamBuilder est un widget reactif qui ecoute un flux (stream) et qui construit automatiquement son
+    //contenu à chaque nouvel evenement (comme ici une mise à jour des message). c'est lui qui gere les changements de données , donc pas besoin d'un Statefulwidget ici
+
+      //le flux ecouté est celui de la collection chat dans firebase. chaque fois qu'un message est ajouté/supprimé/modifier, le stream envoie un evenement, et le StreamBuilder
+      //reconstrui son enfant
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+
+      //tant que la connexion avec firestore est en cours et qu'aucune donnée n'a encore été reçu, on affiche un CircularProgressIndicator
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        //Si aucune données n'est presente (pas encore de messages dans firebase), on affiche un message indiquant qu'il n'ya aucun message
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(child: Text('No messages found.'));
+        }
+        //s'il ya une erreur
+        if (chatSnapshots.hasError) {
+          return const Center(child: Text('Something went wrong...'));
+        }
+
+        //on recupere la liste des documents(messages) presents dans chat
+        final loadedMessages = chatSnapshots.data!.docs;
+
+
+        //on retourne une listeView.builder qui crée dynamiquement un widget Text pour chaque
+        //message recupéré dans la collection
+        return ListView.builder(
+          padding: const EdgeInsets.only(bottom:40,left:13,right:13),
+          reverse: true,
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) =>
+              Text(loadedMessages[index].data()['text']),
+        );
+      },
+    );
   }
 }
--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -18,14 +20,42 @@ class _NewMessage extends State<NewMessage> {
     super.dispose();
   }
 
-  void _submitMessage() {
+  //fonction appelée quand le user veut envoyer un message
+  //asynchrone car on va interagir avec FireStore
+  void _submitMessage() async {
+
+    //recuperation du texte actuellement dans le champ texte du message
     final enteredMessage = _messageController.text;
 
+    //si le message est vide ou ne contient que des espace, on quitte la fonction
+    //pour eviter d'envoyer des messages vides dans la bd
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
-    _messageController.clear();
+    //retire le focus du champs text, pour masquer le clavier après avoir envoyé le message
+    FocusScope.of(context).unfocus();
+    _messageController.clear();//vide le champs texte après envoie, pour le remettre propre pour le prochain message
+
+    //reupere l'utilisateur actuellement connecté via Firebase Auth
+    //le ! signifie qu'on est certain qu'il ya un utilisateur (car sinon cette fonction ne devrait pas etre accessible)
+    final user = FirebaseAuth.instance.currentUser!;
+    //va chercher dans Firestore , dans la collection users, le document coresspondant au user actuel grâce à son uid
+    //parce qu'on a besoin de ses infos associés(username et image) pour les attacher au message
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    //on ajoute un nouveau document dans la collection chat
+    //pour stocker chaque message comme un document independant dans Firestore
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
   }
 
   @override
